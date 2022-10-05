e87517bfcad38c2d670c58ad79671ea9ceff9f29
pmseyeux55 <pmseyeux@gmail.com>
2022-10-05 17:09:18 +0100

    Class Weapon
    1ere ébauche

---
+++ Weapon.py
@@ -0,0 +1,7 @@
+class Weapon():
+    def __init__(self, ammunitions:int, range:int):
+        self._ammunitions = ammunitions
+        self._range = range
+
+    def fire_at(self,x:int,y:int,z:int):
+        self._ammunitions -=1
