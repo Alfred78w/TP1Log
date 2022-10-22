ee1f93335ffc4588a8497b17cfa831f500923242
pmseyeux55 <pmseyeux@gmail.com>
2022-10-22 16:53:30 +0100

    First Try

--- Vessel.py
+++ Vessel.py
@@ -1,2 +1,15 @@
 class Vessel():
-    def __init__(self, coordinates : tuple, max_hits ):
+    def __init__(self, coordinates : tuple, max_hits: int, weapon: Weapon):
+        self.coordinates = coordinates
+        self.max_hits = max_hits
+        self.weapon = Weapon
+
+    def go_to(self,x,y,z):
+        self.coordinates = (x,y,z)
+
+    def get_coordinates(self):
+        print(self.coordinates)
+
+    def fire_at(self,x,y,z):
+        Weapon.fire_at(self.weapon, x,y,z)
+
